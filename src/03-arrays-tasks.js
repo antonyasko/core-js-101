41f5446b9ef685559ba59b8ffada23485308d47f
Anton Yasko <yasko.anton.92@gmail.com>
2019-12-19 10:03:24 +0300

    feat: add test solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -272,8 +272,13 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  if (arr.length > 0) {
+    const aaa = arr.map((item, index) => Array(index + 1).fill(item, 0, index + 1).join(','));
+    const bbb = [];
+    return bbb.concat(aaa).join(',').split(',').map((item) => (item === '' ? null : item));
+  }
+  return arr;
 }
 
 
