195a3e9441c58a622d7af1014e128d1c9ab8e3bc
Anton Yasko <yasko.anton.92@gmail.com>
2019-12-20 12:29:44 +0300

    feat: add test solution

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -171,9 +171,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
-  // return (() => startFrom + 1);
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return function aaa() {
+    count += 1;
+    return count - 1;
+  };
 }
 
 
